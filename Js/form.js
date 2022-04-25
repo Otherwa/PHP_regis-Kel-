5966f03b7dfe3d2ebb32daabda1bc2fdd1b32589
Atharv Desai <67428572+Otherwa@users.noreply.github.com>
2022-04-25 17:37:00 +0530

    Add files via upload

---
+++ Js/form.js
@@ -0,0 +1,21 @@
+// phone number verification
+var phone = document.getElementById("phone");
+var phone_err = document.getElementById("phone_err");
+var phone_err_label = document.getElementById("phone_err_label");
+phone.addEventListener('input', () => {
+    var phoneNumber_err = phone.value;
+    phoneNumber_err = phoneNumber_err.length;
+    if (phoneNumber_err >= 11) {
+        phone.style.color = "red";
+        phone_err_label.style.color = "red";
+        phone.style.borderColor = "red";
+        phone_err.style.display = "block";
+        phone_err_label.phone_err.phone.style.transition = "all 1s";
+    } else {
+        phone.style.color = "#006eff";
+        phone_err_label.style.color = "#006eff";
+        phone.style.borderColor = "#006eff";
+        phone_err.style.display = "none";
+        phone_err_label.phone_err.phone.style.transition = "all 1s";
+    }
+});
--- Php/View/formto.php
+++ Php/View/formto.php
@@ -1,4 +1,3 @@
-
 <?php
 $host = "localhost";
 $user = "root";
@@ -32,6 +31,9 @@ if (isset($_POST['login'])) {
     $review = $_POST['Review'];
     $phone = $_POST['Phone'];
 
+    //val var
+    $phone_val = 10;
+    
     //session_name to send
     $_SESSION['name'] = $name;
 
@@ -43,7 +45,7 @@ if (isset($_POST['login'])) {
     if ($count > 1) {
         //ony two per query user
         echo "<script>alert('Form Instance Already Exists');</script>";
-    } elseif ($name != null && $review != null) {
+    } elseif ($name != null && $review != null && $phone_val >= strlen($phone)) {
         $sql_insert_form_fillup = "INSERT INTO `form_fillup` (`Name`, `Review`, `Date`) VALUES ('$name','$review',current_timestamp());";
         $result = mysqli_query($con, $sql_insert_form_fillup);
 
@@ -80,66 +82,81 @@ if (isset($_POST['login'])) {
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Form</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <!-- tailwind css -->
-        <!-- <script src="https://cdn.tailwindcss.com"></script>  -->
-        <link rel="stylesheet" type="text/css" href="../../dist/output.css" />
-        <link rel="stylesheet" type="text/css" href="../../Css/style.css" />
-        <!-- Gfonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet" />
-        <!-- icon -->
-        <link type="image/png" sizes="16x16" rel="icon" href="https://img.icons8.com/cute-clipart/16/000000/pixel-cat.png" />
-        <!-- num style -->
-        <style>
-            input::-webkit-outer-spin-button,
-            input::-webkit-inner-spin-button {
-                -webkit-appearance: none;
-                margin: 0;
-            }
-            /* Firefox */
-            input[type="number"] {
-                -moz-appearance: textfield;
-            }
-        </style>
-    </head>
-    <body class="p-1 m-0 bg-[#ebebeb]">
+
+<head>
+    <title>Form</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- tailwind css -->
+    <!-- <script src="https://cdn.tailwindcss.com"></script>  -->
+    <link rel="stylesheet" type="text/css" href="../../dist/output.css" />
+    <link rel="stylesheet" type="text/css" href="../../Css/style.css" />
+    <!-- Gfonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet" />
+    <!-- icon -->
+    <link type="image/png" sizes="16x16" rel="icon"
+        href="https://img.icons8.com/cute-clipart/16/000000/pixel-cat.png" />
+    <!-- num style -->
+    <style>
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type="number"] {
+        -moz-appearance: textfield;
+    }
+    </style>
+</head>
+
+<body class="p-1 m-0 bg-[#ebebeb]">
+    <br />
+    <br />
+
+    <div class="l-form p-0">
+        <form action="#" method="POST" class="form">
+            <fieldset>
+                <legend>Yare Yare Daze</legend>
+                <h1 class="form__title" style="font-family: 'Bungee', cursive; font-size: 2.2rem; color: #565656;">
+                    Fill up<br />
+                    <span style="font-size: 1.4rem; font-family: 'Roboto', sans-serif; color: black;">User
+                        &#128466;</span>
+                </h1>
+                <div class="form__div">
+                    <input type="text" class="form__input" name="Name" id="name" placeholder="e.g xyz"
+                        autocomplete="off" />
+                    <label for="" class="form__label">Name</label>
+                </div>
+
+                <div class="form__div">
+                    <input type="text" class="form__input" name="Review" id="review" placeholder="e.g xyz@1"
+                        autocomplete="off" />
+                    <label for="" class="form__label">Review?</label>
+                </div>
+
+                <div class="form__div">
+                    <input type="number" class="form__input" name="Phone" id="phone" placeholder="e.g 8828388979"
+                        autocomplete="off" />
+                    <label for="" class="form__label" id="phone_err_label">Phone</label>
+                </div>
+                <code id="phone_err">- phone number invalid</code>
+
+                <br />
+                <input type="submit" id="sub" class="form__button font-mono" value="Submit" name="login"
+                    style="float: right;" />
+            </fieldset>
+        </form>
         <br />
         <br />
+        <br />
+    </div>
+</body>
 
-        <div class="l-form p-0">
-            <form action="#" method="POST" class="form">
-                <fieldset>
-                    <legend>Yare Yare Daze</legend>
-                    <h1 class="form__title" style="font-family: 'Bungee', cursive; font-size: 2.2rem; color: #565656;">
-                        Fill up<br />
-                        <span style="font-size: 1.4rem; font-family: 'Roboto', sans-serif; color: black;">User &#128466;</span>
-                    </h1>
-                    <div class="form__div">
-                        <input type="text" class="form__input" name="Name" id="name" placeholder="e.g xyz" autocomplete="off" />
-                        <label for="" class="form__label">Name</label>
-                    </div>
+<!-- form validation -->
+<script type="text/javascript" src="../../Js/form.js"></script>
 
-                    <div class="form__div">
-                        <input type="text" class="form__input" name="Review" id="review" placeholder="e.g xyz@1" autocomplete="off" />
-                        <label for="" class="form__label">Review?</label>
-                    </div>
-
-                    <div class="form__div">
-                        <input type="number" class="form__input" name="Phone" id="review" placeholder="e.g 8828388979" autocomplete="off" />
-                        <label for="" class="form__label">Phone</label>
-                    </div>
-                    <br />
-                    <input type="submit" id="sub" class="form__button font-mono" value="Submit" name="login" style="float: right;" onclick="verify()" />
-                </fieldset>
-            </form>
-            <br />
-            <br />
-            <br />
-        </div>
-    </body>
 </html>

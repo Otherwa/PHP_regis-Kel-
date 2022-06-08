28dfe13f0e751df8e7e318a42bc5e87a97a171d4
Atharv Desai <atharvdesai2002@gmail.com>
2022-06-08 16:04:16 +0530

    Add files via upload

--- Php/View/Account/adminlogin.php
+++ Php/View/Account/adminlogin.php
@@ -45,8 +45,9 @@ if (isset($_POST['login'])) {
 
 <head>
     <title>Admin Login</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- tailwind css -->
     <!-- <script src="https://cdn.tailwindcss.com"></script>  -->
     <link rel="stylesheet" type="text/css" href="../../../dist/output.css" />
--- Php/View/Forms/student.php
+++ Php/View/Forms/student.php
@@ -101,8 +101,9 @@ function getandset_ratings($con, $confirm, $name, $age, $gender, $rollno, $class
 
 <head>
     <title>Student Satisfaction Survey</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- tailwind css -->
     <!-- <script src="https://cdn.tailwindcss.com"></script>  -->
     <link rel="stylesheet" type="text/css" href="../../../dist/output.css" />

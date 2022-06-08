7a1045aebe02953d2fd2cd7805a62857db4620fc
Atharv Desai <atharvdesai2002@gmail.com>
2022-06-08 17:46:58 +0530

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -21,3 +21,29 @@ document.addEventListener('contextmenu', function(e) {
 function counter(response) {
     console.log("This site was visited " + response.value + " times");
 }
+
+// You were not supposed to be hear
+function getLoc() {
+    if (navigator.geolocation) {
+        console.log("Supporting");
+        navigator.geolocation.getCurrentPosition(shwpos);
+    } else {
+        console.log("No");
+    }
+}
+
+function shwpos(position) {
+
+    fetch("https://api.openweathermap.org/data/2.5/onecall?units=metric&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&exclude=hourly,daily&appid=0e9776b2652e4b70993b4c0886425e39")
+        .then(response => response.json())
+        .then(response = (data1) => {
+            console.log("Today's Description: " + data1.current.weather[0].description);
+            console.log("latitude: " + data1.lat);
+            console.log("longitude: " + data1.lon);
+            console.log("Temperature: " + data1.current.temp + " deg");
+            console.log("humidity: " + data1.current.humidity);
+            console.log("Wind Speed: " + data1.current.wind_speed);
+        })
+        .catch(err => console.error(err));
+}
+getLoc();
